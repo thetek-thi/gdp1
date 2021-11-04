641e10f34275a314ff43f82b57c9b534da410b72
thetek-thi <git-thi@thetek.de>
2021-11-04 17:58:34 +0100

    add rest of blatt3 code

--- blatt3/worm.c
+++ blatt3/worm.c
@@ -31,8 +31,8 @@
 
 
 
-int worm_pos_head_y, // y coord of worm head
-    worm_pos_head_x, // x coord of worm head
+int worm_head_y, // y coord of worm head
+    worm_head_x, // x coord of worm head
     worm_dx,         // heading of worm
     worm_dy,         // heading of worm
     worm_wcolor;
@@ -68,7 +68,7 @@ init_colors (void)
 
 // read user input
 void
-read_user_input (int* game_start)
+read_user_input (int* game_state)
 {
     int ch; // for storing the key codes
 
@@ -81,8 +81,8 @@ read_user_input (int* game_start)
             case KEY_DOWN:  set_worm_heading (WORM_DOWN);  break;
             case KEY_LEFT:  set_worm_heading (WORM_LEFT);  break;
             case KEY_RIGHT: set_worm_heading (WORM_RIGHT); break;
-            case 's': /* TODO: SINGLE STEP */ break; // single step
-            case ' ': /* TODO: TERMINATE SIGNLE STEP */ break; // terminate signle step
+            case 's': nodelay (stdscr, FALSE); break; // single step
+            case ' ': nodelay (stdscr, TRUE);  break; // terminate signle step
         }
     }
 
@@ -115,7 +115,24 @@ do_level ()
     end_level_loop = FALSE;
     while (end_level_loop == FALSE)
     {
-        /* TODO: THIS STUFF RIGHT HERE */
+        read_user_input (&game_state);
+        if (game_state == WORM_STATUS_QUIT)
+        {
+            end_level_loop = TRUE;
+            continue;
+        }
+
+        move_worm (&game_state);
+
+        if (game_state != WORM_STATUS_ONGOING)
+        {
+            end_level_loop = TRUE;
+            continue;
+        }
+
+        show_worm ();
+        napms (NAP_TIME);
+        refresh ();
     }
 
     res_code = EXIT_SUCCESS;
@@ -124,7 +141,134 @@ do_level ()
 
 
 
-/*
- * TODO: REST OF THE PROGRAM
- */
+// ncurses initializer
+void
+app_init ()
+{
+    initscr ();
+    noecho (); cbreak (); nonl (); curs_set (0);
+    keypad (stdscr, TRUE); nodelay (stdscr, TRUE);
+}
+
+
+
+// ncurses deinitializer
+void
+app_deinit ()
+{
+    standend ();
+    refresh ();
+    curs_set (1);
+    endwin ();
+}
+
+
+
+// place item on board
+void
+place_item (int    y,
+            int    x,
+            chtype symbol,
+            int    color_pair)
+{
+    move (y, x);
+    attron (COLOR_PAIR (color_pair));
+    addch (symbol);
+    attroff (COLOR_PAIR (color_pair));
+}
+
+
+
+// getters
+
+int get_last_row () { return LINES-1; }
+int get_last_col () { return COLS-1;  }
+
+
+
+// initialize the worm
+int
+init_worm (int head_y,
+           int head_x,
+           int dir,
+           int color)
+{
+    worm_head_y = head_y;
+    worm_head_x = head_x;
+    
+    set_worm_heading (dir);
+
+    worm_wcolor = color;
+
+    return EXIT_SUCCESS;
+}
+
+
+
+// show the worms elements on display
+void
+show_worm ()
+{
+    place_item (
+        worm_head_y,
+        worm_head_x,
+        WORM_SYMBOL,
+        worm_wcolor
+    );
+}
+
+
+
+// move worm
+void
+move_worm (int* game_state)
+{
+    worm_head_y += worm_dy;
+    worm_head_x += worm_dx;
+
+    if (worm_head_x < 0 || worm_head_x > get_last_col () ||
+        worm_head_y < 0 || worm_head_y > get_last_row ())
+    {
+        *game_state = WORM_STATUS_OOB;
+    }
+}
+
+
+
+// setters
+
+void
+set_worm_heading (int dir)
+{
+    switch (dir)
+    {
+        case WORM_UP:    worm_dx =  0; worm_dy = -1; break;
+        case WORM_DOWN:  worm_dx =  0; worm_dy =  1; break;
+        case WORM_LEFT:  worm_dx = -1; worm_dy =  0; break;
+        case WORM_RIGHT: worm_dx =  1; worm_dy =  0; break;
+    }
+}
+
+
+
+int
+main (void)
+{
+    int res_code;
+
+    app_init ();
+    init_colors ();
+
+    if (LINES < MIN_ROWS || COLS < MIN_COLS)
+    {
+        app_deinit ();
+        printf ("window too small - we need at least %dx%d\n", MIN_COLS, MIN_ROWS);
+        res_code = EXIT_FAILURE;   
+    } else {
+        res_code = do_level ();
+        app_deinit ();
+    }
+
+    return res_code;
+}
 
